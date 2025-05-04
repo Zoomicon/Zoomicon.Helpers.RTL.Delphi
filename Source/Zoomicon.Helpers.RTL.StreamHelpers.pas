92f5b34340eb86131e1e7b38c58034fe69656912
birbilis <birbilis@zoomicon.com>
2025-05-04 19:14:49 +0300

    - Added TStreamPreambleHelper helper class - Bumped BOSS package version to 3.3

--- Source/Zoomicon.Helpers.RTL.StreamHelpers.pas
+++ Source/Zoomicon.Helpers.RTL.StreamHelpers.pas
@@ -16,9 +16,18 @@ interface
       function ReadAllText(const DefaultEncoding: TEncoding = nil; const ForceDefaultEncoding: Boolean = false): string;
     end;
 
+    TStreamPreambleHelper = class helper(TStreamReadAllHelper) for TStream
+      function ReadBytes(const MaxBytes: Integer): TBytes;
+      function PeekBytes(const MaxBytes: Integer): TBytes;
+      function PeekString(const MaxBytes: Integer; Encoding: TEncoding = nil): String;
+      procedure SkipBOM_UTF8;
+      //TODO: Add SkipBOM_UTF16_BE (Big-Endian), SkipBOM_UTF16_LE (Little-Endian)
+    end;
+
 implementation
   uses
-    RTLConsts; //for SFileTooLong
+    System.RTLConsts, //for SFileTooLong
+    System.Math; //for Min
 
 {$REGION 'TStreamErrorHelper'}
 
@@ -58,6 +67,44 @@ end;
       FoundEncoding := nil; //doc for TEncoding.GetBufferEncoding writes: "The AEncoding parameter should have the value NIL, otherwise its value is used to detect the encoding" //Note: Delphi doesn't auto-initialize local variables, so we need to explicitly set to nil
     var BOMLength := TEncoding.GetBufferEncoding(Buff, FoundEncoding, DefaultEncoding);
     result := FoundEncoding.GetString(Buff, BOMLength, Length(Buff) - BOMLength); //TODO: not sure if this will do transcoding if the real encoding of the file was different from DefaultEncoding and we used ForceDefaultEncoding=true - may need to implement it somehow
+  end;
+
+{$ENDREGION}
+
+{$REGION 'TStreamPreambleHelper'}
+
+  function TStreamPreambleHelper.ReadBytes(const MaxBytes: Integer): TBytes;
+  begin
+    var LReadLen := Min(MaxBytes, Size - Position);
+    SetLength(Result, LReadLen);
+    ReadBuffer(Result[0], LReadLen);
+  end;
+
+  function TStreamPreambleHelper.PeekBytes(const MaxBytes: Integer): TBytes;
+  begin
+    var LOldPos := Position;  //save current position
+    try
+      Result := ReadBytes(MaxBytes);
+    finally
+      Position := LOldPos;  //restore position
+    end;
+  end;
+
+  function TStreamPreambleHelper.PeekString(const MaxBytes: Integer; Encoding: TEncoding = nil): String;
+  begin
+    if Encoding = nil then
+      Encoding := TEncoding.UTF8;  // Default to UTF-8
+
+    var LBytes := PeekBytes(MaxBytes);  // Use byte peek function
+    Result := Encoding.GetString(LBytes);  // Convert to string
+  end;
+
+  procedure TStreamPreambleHelper.SkipBOM_UTF8;
+  begin
+    var LStreamStartPos := Position;
+    const UTF8_BOM = TBytes.Create($EF, $BB, $BF);
+    if not (ReadBytes(Length(UTF8_BOM)) = UTF8_BOM) then //if not starting with UTF8 BOM...
+      Position := LStreamStartPos; //...go back to original position
   end;
 
 {$ENDREGION}
